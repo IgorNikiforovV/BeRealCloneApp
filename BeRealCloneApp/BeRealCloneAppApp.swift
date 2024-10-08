efd25b2acab0e00acc3138a5662cba992426b97b
Igor Nikiforov <inikiforovj7@icloud.com>
2024-10-08 11:26:20 +0500

    add empty project

---
+++ BeRealCloneApp/BeRealCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealCloneAppApp.swift
+//  BeRealCloneApp
+//
+//  Created by Игорь Никифоров on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BeRealCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
