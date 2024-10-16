89435f2a0189ae0f427f5c8a2edbdba31e3861be
Igor Nikiforov <inikiforovj7@icloud.com>
2024-10-16 11:23:00 +0500

    Task3 added FeedCell to Feed screen

--- BeRealCloneApp/Feed.swift
+++ BeRealCloneApp/Feed.swift
@@ -12,79 +12,85 @@ struct Feed: View {
         ZStack {
             Color.black
                 .ignoresSafeArea()
-            ZStack {
-                VStack {
-                    VStack {
-                        ZStack {
-                            VStack(alignment: .leading) {
-                                Image("back")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .cornerRadius(5)
-                            }
-
+                ZStack {
+                    ScrollView {
+                        VStack {
                             VStack {
-                                HStack {
-                                    Image("me")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .border(.black)
-                                        .cornerRadius(2)
-                                        .frame(width: 20, height: 40)
-                                        .padding(.leading)
-                                    Spacer()
+                                ZStack {
+                                    VStack(alignment: .leading) {
+                                        Image("back")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .cornerRadius(5)
+                                    }
+                                    
+                                    VStack {
+                                        HStack {
+                                            Image("me")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .border(.black)
+                                                .cornerRadius(2)
+                                                .frame(width: 20, height: 40)
+                                                .padding(.leading)
+                                            Spacer()
+                                        }
+                                        .padding(.top, 18)
+                                        Spacer()
+                                    }
                                 }
-                                .padding(.top, 18)
-                                Spacer()
+                                .frame(width: 100)
+                            }
+                            VStack {
+                                Text("Add a caption...")
+                                    .foregroundStyle(.white)
+                                    .fontWeight(.semibold)
+                                Text("View Comment")
+                                    .foregroundStyle(.gray)
+                                HStack {
+                                    Text("Perm, Perm-South • 1 hr late")
+                                        .foregroundStyle(.gray)
+                                        .font(.system(size: 12))
+                                    ThreeDots(size: 3, color: .gray)
+                                }
+                            }
+                            ForEach(1..<8) { _ in
+                                FeedCell()
                             }
                         }
-                        .frame(height: 100)
+                        .padding(.top, 80)
                     }
+
                     VStack {
-                        Text("Add a caption...")
-                            .foregroundStyle(.white)
-                            .fontWeight(.semibold)
-                        Text("View Comment")
-                            .foregroundStyle(.gray)
-                        HStack {
-                            Text("Perm, Perm-South • 1 hr late")
-                                .foregroundStyle(.gray)
-                                .font(.system(size: 12))
-                            ThreeDots(size: 3, color: .gray)
+                        VStack {
+                            HStack {
+                                Image(systemName: "person.2.fill")
+                                    .foregroundColor(.white)
+                                Spacer()
+
+                                Text("BeReal")
+                                    .foregroundStyle(.white)
+                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                    .font(.system(size: 22))
+                                Spacer()
+
+                                Image("photo")
+                                    .resizable()
+                                    .frame(width: 35, height: 35)
+                                    .cornerRadius(17.5)
+                            }
+                            .padding(.horizontal)
+                            HStack {
+                                Text("My Friends")
+                                    .foregroundStyle(.white)
+                                    .fontWeight(.semibold)
+                                Text("Discovery")
+                                    .foregroundStyle(.gray)
+                            }
                         }
+                        Spacer()
                     }
                 }
-
-                VStack {
-                    VStack {
-                        HStack {
-                            Image(systemName: "person.2.fill")
-                                .foregroundColor(.white)
-                            Spacer()
-
-                            Text("BeReal")
-                                .foregroundStyle(.white)
-                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                .font(.system(size: 22))
-                            Spacer()
-
-                            Image("photo")
-                                .resizable()
-                                .frame(width: 35, height: 35)
-                                .cornerRadius(17.5)
-                        }
-                        .padding(.horizontal)
-                        HStack {
-                            Text("My Friends")
-                                .foregroundStyle(.white)
-                                .fontWeight(.semibold)
-                            Text("Discovery")
-                                .foregroundStyle(.gray)
-                        }
-                    }
-                    Spacer()
-                }
-            }
         }
     }
 }
