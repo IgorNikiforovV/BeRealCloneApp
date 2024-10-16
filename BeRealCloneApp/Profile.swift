7ef17cdfdbb6a8bcb340f00cc49ec54ef70bd90a
Igor Nikiforov <inikiforovj7@icloud.com>
2024-10-16 11:28:37 +0500

    Task4 added empty profile file

---
+++ BeRealCloneApp/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  BeRealCloneApp
+//
+//  Created by Игорь Никифоров on 16.10.2024.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile()
+}
