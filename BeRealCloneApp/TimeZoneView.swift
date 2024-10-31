f3ab13424399085121ad903d0a4b0300e75f8965
Igor Nikiforov <inikiforovj7@icloud.com>
2024-10-31 11:36:33 +0500

    Task7 finished TimeZoneView screen

--- BeRealCloneApp/TimeZoneView.swift
+++ BeRealCloneApp/TimeZoneView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct TimeZoneView: View {
+
+    @State var area = "europe"
+
     var body: some View {
         VStack {
             ZStack {
@@ -55,13 +58,13 @@ struct TimeZoneView: View {
                     VStack {
                         ZStack {
                             RoundedRectangle(cornerRadius: 14)
-                                .frame(width: UIScreen.main.bounds.width * 0.9, height: 190)
+                                .frame(width: UIScreen.main.bounds.width * 0.9, height: 193)
                                 .foregroundColor(.white)
                                 .opacity(0.07 )
 
                             VStack {
                                 Button {
-
+                                    self.area = "europe"
                                 } label: {
                                     HStack {
                                         Image(systemName: "globe.europe.africa.fill")
@@ -70,13 +73,122 @@ struct TimeZoneView: View {
                                             .foregroundColor(.white)
                                             .fontWeight(.semibold)
                                         Spacer()
+                                        if self.area == "europe" {
+                                            Image(systemName: "checkmark.circle")
+                                                .foregroundColor(.gray)
+                                                .font(.system(size: 14))
+                                        }
                                     }
+                                    .padding(.horizontal, UIScreen.main.bounds.width * 0.05)
+                                    .frame(height: 30)
                                 }
 
+                                HStack {
+                                    Spacer()
+                                    Rectangle()
+                                        .frame(width: UIScreen.main.bounds.width * 0.8, height: 0.3)
+                                        .opacity(0.4)
+                                    .foregroundColor(.gray)
+                                }
+
+                                Button {
+                                    self.area = "americas"
+                                } label: {
+                                    HStack {
+                                        Image(systemName: "globe.americas.fill")
+                                            .foregroundColor(.white)
+                                        Text("Americas")
+                                            .foregroundColor(.white)
+                                            .fontWeight(.semibold)
+                                        Spacer()
+                                        if self.area == "americas" {
+                                            Image(systemName: "checkmark.circle")
+                                                .foregroundColor(.gray)
+                                                .font(.system(size: 14))
+                                        }
+                                    }
+                                    .padding(.horizontal, UIScreen.main.bounds.width * 0.05)
+                                    .frame(height: 30)
+                                }
+
+                                HStack {
+                                    Spacer()
+                                    Rectangle()
+                                        .frame(width: UIScreen.main.bounds.width * 0.8, height: 0.3)
+                                        .opacity(0.4)
+                                    .foregroundColor(.gray)
+                                }
+
+                                Button {
+                                    self.area = "eastasia"
+                                } label: {
+                                    HStack {
+                                        Image(systemName: "globe.asia.australia.fill")
+                                            .foregroundColor(.white)
+                                        Text("East Asia")
+                                            .foregroundColor(.white)
+                                            .fontWeight(.semibold)
+                                        Spacer()
+                                        if self.area == "eastasia" {
+                                            Image(systemName: "checkmark.circle")
+                                                .foregroundColor(.gray)
+                                                .font(.system(size: 14))
+                                        }
+                                    }
+                                    .padding(.horizontal, UIScreen.main.bounds.width * 0.05)
+                                    .frame(height: 30)
+                                }
+
+                                HStack {
+                                    Spacer()
+                                    Rectangle()
+                                        .frame(width: UIScreen.main.bounds.width * 0.8, height: 0.3)
+                                        .opacity(0.4)
+                                    .foregroundColor(.gray)
+                                }
+
+                                Button {
+                                    self.area = "westasia"
+                                } label: {
+                                    HStack {
+                                        Image(systemName: "globe.asia.australia.fill")
+                                            .foregroundColor(.white)
+                                        Text("West Asia")
+                                            .foregroundColor(.white)
+                                            .fontWeight(.semibold)
+                                        Spacer()
+                                        if self.area == "westasia" {
+                                            Image(systemName: "checkmark.circle")
+                                                .foregroundColor(.gray)
+                                                .font(.system(size: 14))
+                                        }
+                                    }
+                                    .padding(.horizontal, UIScreen.main.bounds.width * 0.05)
+                                    .frame(height: 30)
+                                }
                             }
                         }
+                        .padding(.top)
                     }
+
+                    Spacer()
+
+                    Button {
+
+                    } label: {
+                        RoundedRectangle(cornerRadius: 14)
+                            .frame(width: UIScreen.main.bounds.width * 0.9, height: 45)
+                            .foregroundStyle(Color(red: 86/255, green: 86/255, blue: 88/255))
+                            .overlay {
+                                Text("Save")
+                                    .foregroundStyle(.black)
+                                    .font(.system(size: 14))
+                            }
+                    }
+
                 }
+                .padding(.top, 50)
+                .padding(.horizontal)
             }
         }
     }
