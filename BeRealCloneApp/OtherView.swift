1524a0c360bc5f7b9a326b41c33e84dc77930e4c
Igor Nikiforov <inikiforovj7@icloud.com>
2024-11-15 11:34:47 +0500

    Task14 Finished OtherView

--- BeRealCloneApp/OtherView.swift
+++ BeRealCloneApp/OtherView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct OtherView: View {
-    @State var toggle = false
+    @State var fastCamera = false
 
     var body: some View {
         VStack {
@@ -47,12 +47,55 @@ struct OtherView: View {
                                     .foregroundStyle(.white)
                                     .font(.system(size: 18))
                                     Spacer()
-                                Toggle(isOn: $toggle) {
-
-                                }
+                                Toggle(isOn: $fastCamera) {}
                             }
+                            .padding(.horizontal, UIScreen.main.bounds.width * 0.04)
+                            .frame(height: 30)
+                        }
+
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(height: 45)
+                                .foregroundColor(Color(red: 28/255, green: 28/255, blue: 30/255))
+
+                            HStack {
+                                Image(systemName: "xmark.app")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 14))
+                                Text("Clear cache")
+                                    .foregroundStyle(.white)
+                                    .font(.system(size: 18))
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 14))
+                            }
+                            .padding(.horizontal, UIScreen.main.bounds.width * 0.04)
+                            .frame(height: 30)
+                        }
+
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(height: 45)
+                                .foregroundColor(Color(red: 28/255, green: 28/255, blue: 30/255))
+
+                            HStack {
+                                Spacer()
+
+                                Text("Delete Account")
+                                    .foregroundStyle(.red)
+
+                                Spacer()
+                            }
+                            .padding(.horizontal, UIScreen.main.bounds.width * 0.04)
+                            .frame(height: 30)
                         }
                     }
+                    .padding(.horizontal)
+                    .padding(.top, 50)
+
+                    Spacer()
                 }
             }
         }
