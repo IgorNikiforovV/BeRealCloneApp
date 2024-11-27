85be8c203c747ca89fbc568708d0cda10c8dc8dc
Igor Nikiforov <inikiforovj7@icloud.com>
2024-11-27 23:04:26 +0500

    Task21 Added Country files

---
+++ BeRealCloneApp/Countries/Country.swift
@@ -0,0 +1,41 @@
+//
+//  Country.swift
+//  BeRealCloneApp
+//
+//  Created by Игорь Никифоров on 27.11.2024.
+//
+
+import Foundation
+import SwiftUI
+
+public struct Country: Codable {
+    public var phoneCode: String
+    public let isoCode: String
+
+    public init(phoneCode: String, isoCode: String) {
+        self.phoneCode = phoneCode
+        self.isoCode = isoCode
+    }
+
+    public init(isoCode: String) {
+        self.isoCode = isoCode
+        phoneCode = ""
+        if let country = Self.allCountries.first(where: { $0.isoCode == isoCode
+        }) {
+            phoneCode = country.phoneCode
+        }
+    }
+
+    static let allCountries: [Country] = Bundle.main.decode(file: "countries.json")
+}
+
+public extension Country {
+    /// Returns localized country name for localeIdentifier
+    var localizedName: String {
+        let id = NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.countryCode.rawValue: isoCode])
+        let name = NSLocale(localeIdentifier: NSLocale.current.identifier)
+            .displayName(forKey: NSLocale.Key.identifier, value: id) ?? isoCode
+        return name
+    }
+}
+
