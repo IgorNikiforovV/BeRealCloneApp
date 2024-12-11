a53f600c7d83be255c576a8c91a29fd714a9af8e
Igor Nikiforov <inikiforovj7@icloud.com>
2024-12-11 23:55:13 +0500

    Task27 Added. MainView

---
+++ BeRealCloneApp/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  MainView.swift
+//  BeRealCloneApp
+//
+//  Created by Игорь Никифоров on 11.12.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @EnvironmentObject var viewModel: AuthenticationViewModel
+
+    var body: some View {
+        Group {
+            if viewModel.userSession == nil {
+                MainAuthenticationView()
+            } else {
+                Text("Home")
+            }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
